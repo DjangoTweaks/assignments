0deb780504320e91e81a3d7ed5c33dfa822f8b52
DjangoTweaks <djangotweaks@gmail.com>
2024-06-14 17:31:19 +0530

    assignment 1 useMemo Completed

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
 export function Assignment1() {
-    const [input, setInput] = useState(0);
-    // Your solution starts here
-    const expensiveValue = 0; 
-    // Your solution ends here
+
+const [input, setInput] = useState(0);
+
+    
+
+const expensiveValue = useMemo(() => expensiveCalculation(input), [input]);
 
     return (
         <div>
@@ -20,3 +22,29 @@ export function Assignment1() {
         </div>
     );
 }
+
+
+const expensiveCalculation = (input)=> 
+{
+    let ans = 1; 
+
+    if(input==0 || input ==1)
+    {
+        return 1; 
+    }
+    else
+    {
+
+    for(let i = 2; i<=input; i++)
+    {
+        ans = ans*i; 
+    }
+
+    return ans; 
+
+    }
+}
+
+
+
+
